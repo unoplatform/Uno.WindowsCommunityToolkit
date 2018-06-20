11a7ce8e5e42274bdc72cee3ed69c13210093461
Jérôme Laban <jerome.laban@nventive.com>
2018-06-19 22:55:56 -0400

    Add services support for Uno

--- Microsoft.Toolkit.Services/Services/OneDrive/Platform/OneDriveServicePlatform.cs
+++ Microsoft.Toolkit.Services/Services/OneDrive/Platform/OneDriveServicePlatform.cs
@@ -1,3 +1,4 @@
+#if WINRT
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
@@ -75,3 +76,4 @@ namespace Microsoft.Toolkit.Services.OneDrive.Uwp
         }
     }
 }
+#endif
