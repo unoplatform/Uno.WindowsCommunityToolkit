436f80aeb26f090dbfd7ef16d3dfdd5be6f15e7e
Xiaoy312 <xiaoyao312@gmail.com>
2019-09-18 13:55:33 -0400

    fixed demo area being covered by info area in narrow-state

---
+++ Microsoft.Toolkit.Uwp.SampleApp.Shared/Triggers/PaneStateTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Toolkit.Uwp.SampleApp.Models;
+using Windows.UI.Xaml;
+
+namespace Microsoft.Toolkit.Uwp.SampleApp.Shared.Triggers
+{
+    public class PaneStateTrigger : StateTriggerBase
+	{
+        #region Property: Binding
+        public static readonly DependencyProperty BindingProperty = DependencyProperty.Register(
+            nameof(Binding),
+            typeof(PaneState),
+            typeof(PaneStateTrigger),
+            new PropertyMetadata(default(PaneState), (s, e) => (s as PaneStateTrigger)?.UpdateTrigger()));
+
+        public PaneState Binding
+        {
+            get => (PaneState)GetValue(BindingProperty);
+            set => SetValue(BindingProperty, value);
+        }
+        #endregion
+
+        #region Property: Value
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(Value),
+            typeof(PaneState),
+            typeof(PaneStateTrigger),
+            new PropertyMetadata(default(PaneState), (s, e) => (s as PaneStateTrigger)?.UpdateTrigger()));
+
+        public PaneState Value
+        {
+            get => (PaneState)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+        #endregion
+
+        private void UpdateTrigger() => SetActive(Binding == Value);
+    }
+}
