04222ca47d46db92bd4a842a26d8b8991977a0a7
Guillaume ENIONA <guillaume.eniona@outlook.com>
2020-09-30 13:17:27 -0400

    Fix unoui include in csproj

--- Microsoft.Toolkit.Uwp.SampleApp/SamplePages/TokenizingTextBox/TokenizingTextBoxPage.xaml.cs
+++ Microsoft.Toolkit.Uwp.SampleApp/SamplePages/TokenizingTextBox/TokenizingTextBoxPage.xaml.cs
@@ -209,11 +209,11 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
             // TODO: Add InApp Notification?
             if (data is SampleDataType sample)
             {
-                //Debug.WriteLine("Added Token: " + sample.Text);
+                System.Diagnostics.Debug.WriteLine("Added Token: " + sample.Text);
             }
             else
             {
-                //Debug.WriteLine("Added Token: " + data);
+                System.Diagnostics.Debug.WriteLine("Added Token: " + data);
             }
         }
 
@@ -221,11 +221,11 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
         {
             if (args.Item is SampleDataType sample)
             {
-                //Debug.WriteLine("Removed Token: " + sample.Text);
+                System.Diagnostics.Debug.WriteLine("Removed Token: " + sample.Text);
             }
             else
             {
-                //Debug.WriteLine("Removed Token: " + args.Item);
+                System.Diagnostics.Debug.WriteLine("Removed Token: " + args.Item);
             }
         }
 
@@ -258,11 +258,11 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
         {
             if (args is SampleEmailDataType sample)
             {
-                //Debug.WriteLine("Added Email: " + sample.DisplayName);
+                System.Diagnostics.Debug.WriteLine("Added Email: " + sample.DisplayName);
             }
             else
             {
-                //Debug.WriteLine("Added Token: " + args);
+                System.Diagnostics.Debug.WriteLine("Added Token: " + args);
             }
 
             _acvEmail.RefreshFilter();
@@ -272,11 +272,11 @@ namespace Microsoft.Toolkit.Uwp.SampleApp.SamplePages
         {
             if (args is SampleEmailDataType sample)
             {
-                //Debug.WriteLine("Removed Email: " + sample.DisplayName);
+                System.Diagnostics.Debug.WriteLine("Removed Email: " + sample.DisplayName);
             }
             else
             {
-                //Debug.WriteLine("Removed Token: " + args);
+                System.Diagnostics.Debug.WriteLine("Removed Token: " + args);
             }
 
             _acvEmail.RefreshFilter();
