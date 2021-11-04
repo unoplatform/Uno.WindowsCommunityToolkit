619279c70bd8fa081b983a0c35adccdad65a28e9
Jerome Laban <jerome.laban@nventive.com>
2021-11-04 19:38:33 -0400

    chore: Adjust datagrid resources

--- CommunityToolkit.WinUI.UI.Controls.DataGrid/Properties/Resources.Designer.cs
+++ CommunityToolkit.WinUI.UI.Controls.DataGrid/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CommunityToolkit.WinUI.UI.Controls {
+namespace CommunityToolkit.WinUI.UI.Controls.DG {
     using System;
     
     
@@ -19,19 +19,19 @@ namespace CommunityToolkit.WinUI.UI.Controls {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
-
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
@@ -39,13 +39,13 @@ namespace CommunityToolkit.WinUI.UI.Controls {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CommunityToolkit.WinUI.UI.Controls.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CommunityToolkit.WinUI.UI.Controls.DG.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
@@ -59,7 +59,7 @@ namespace CommunityToolkit.WinUI.UI.Controls {
                 resourceCulture = value;
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to row.
         /// </summary>
@@ -68,7 +68,7 @@ namespace CommunityToolkit.WinUI.UI.Controls {
                 return ResourceManager.GetString("DataGridRowAutomationPeer_ItemType", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to ({0} items).
         /// </summary>
@@ -77,7 +77,7 @@ namespace CommunityToolkit.WinUI.UI.Controls {
                 return ResourceManager.GetString("DataGridRowGroupHeader_ItemCountPlural", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to ({0} item).
         /// </summary>
@@ -86,7 +86,7 @@ namespace CommunityToolkit.WinUI.UI.Controls {
                 return ResourceManager.GetString("DataGridRowGroupHeader_ItemCountSingular", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to {0}:.
         /// </summary>
@@ -95,7 +95,7 @@ namespace CommunityToolkit.WinUI.UI.Controls {
                 return ResourceManager.GetString("DataGridRowGroupHeader_PropertyName", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Group.
         /// </summary>
