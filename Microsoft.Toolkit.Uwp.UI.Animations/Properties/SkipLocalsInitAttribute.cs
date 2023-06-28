d8450c0ecf8184d54d8bf431f011b7668cbea656
Jerome Laban <jerome.laban@nventive.com>
2023-06-28 10:08:02 -0400

    ci: Adjust warnings and missing attribute

--- Microsoft.Toolkit.Uwp.UI.Animations/Properties/SkipLocalsInitAttribute.cs
+++ Microsoft.Toolkit.Uwp.UI.Animations/Properties/SkipLocalsInitAttribute.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if WINDOWS_UWP
 namespace System.Runtime.CompilerServices
 {
     /// <summary>
@@ -22,3 +23,4 @@ namespace System.Runtime.CompilerServices
     {
     }
 }
+#endif
